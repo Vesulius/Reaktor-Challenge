c86342e23ae2851992d32e1e204cec9329ccba52
vesa <vesainwonderland@gmail.com>
2022-01-13 19:08:43 +0200

    display game results

--- src/components/LiveList.js
+++ src/components/LiveList.js
@@ -56,9 +56,21 @@ const DisplayGame = game => {
       <h6>
         {game.playerA} VS {game.playerB}
       </h6>
+      <ol>{getResult(game)}</ol>
     </div>
   )
 }
 
+// the order of this list matters for getResult function
+const results = ['ROCK', 'PAPER', 'SCISSORS']
+
+const getResult = game => {
+  if (game.result === null) return 'Playing...'
+  const resultA = results.indexOf(game.result.playsA)
+  const resultB = results.indexOf(game.result.playsB)
+  if (resultA === resultB) return 'DRAW'
+  if (resultA + 1 === resultB || !(resultA - 1 === resultB))  return `Winner ${game.playerB}`
+  return `Winner ${game.playerA}`
+}
 
 export default LiveList
